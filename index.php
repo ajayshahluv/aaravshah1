a46a4c1985b98c1b218ba91059240cba0fbfbc93
ajayshahluv <56653356+ajayshahluv@users.noreply.github.com>
2021-07-10 20:35:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>My website </title>
+<style>
+body {
+  background-image: url('hb.jpg');
+background-size: 1500px 630px;
+background-repeat: no-repeat;
+} 
+p{font-size:35}
+</style>
+<H1>Welcome to my website</H1>
+<p><b>Hello my name is Aarav shah my birthday is coming on 22nd July.</b></p>
+<H1>I WILL BE 9 YEARS ON MY BIRTHDAY </H1>
+</body>
+</head>
+</html>
